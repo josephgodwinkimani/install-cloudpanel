8cffd3703d03c08ddf69d34ef7717464ce9b196b
josephgodwinkimani <13916761+josephgodwinkimani@users.noreply.github.com>
2022-12-14 20:36:44 +0300

    Update mailserver.sh

--- mailserver.sh
+++ mailserver.sh
@@ -44,6 +44,8 @@ echo "What is your timezone? (e.g +3.00) "
 read TIMEZONE
 mysql --user="root" --password="$ROOTPASSWORD" --execute="SET GLOBAL time_zone = '$TIMEZONE'; SELECT @@global.time_zone;"
 
+sudo service mysql restart
+
 mysql --user="root" --password="$ROOTPASSWORD" <<EOF
 CREATE USER 'mailuser'@'127.0.0.1' IDENTIFIED BY '$PASSWORD'; 
 GRANT SELECT ON mailserver.* TO 'mailuser'@'127.0.0.1';
