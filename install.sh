7d47232483ad8da5b6c74bccc9e28b4334b3bd34
josephgodwinkimani <13916761+josephgodwinkimani@users.noreply.github.com>
2022-12-14 13:41:02 +0300

    check ram requirements

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ log_info() {
   printf "\n\e[0;35m $1\e[0m\n\n"
 }
 
+RAM=`echo $(($(getconf _PHYS_PAGES) * $(getconf PAGE_SIZE) / (1024 * 1024)))`
+
 NGINX=`nginx --version`
 MYSQL=`mysql --version`
 NODE=`node --version`
@@ -14,42 +16,62 @@ PYTHON=`python --version`
 PYTHON3=`python3 --version`
 PHP=`php --version`
 REDIS=`redis-server --version`
+VARNISH=`varnish --version`
 
 log_info "Checking your system ..."
+
+if [[ $RAM <= 1999 ]]; then
+    echo "You have $RAM available"
+    echo "For the installation, you need at least 2GB Memory - https://www.cloudpanel.io/docs/v2/requirements/#memory"
+    exit
+fi
 
 if [[ $NGINX ]]; then
     echo "You have $NGINX installed"
     echo "For the installation, you need an empty server with Ubuntu 22.04 or Debian 11 with root access - https://www.cloudpanel.io/docs/v2/technology-stack/"
+    exit
 fi
 
-if [[ $NGINX ]]; then
+if [[ $MYSQL ]]; then
     echo "You have $MYSQL installed"
     echo "For the installation, you need an empty server with Ubuntu 22.04 or Debian 11 with root access - https://www.cloudpanel.io/docs/v2/technology-stack/"
+    exit
 fi
 
 if [[ $NODE ]]; then
     echo "You have $NODE installed"
     echo "For the installation, you need an empty server with Ubuntu 22.04 or Debian 11 with root access - https://www.cloudpanel.io/docs/v2/technology-stack/"
+    exit
 fi
 
 if [[ $PYTHON ]]; then
     echo "You have $PYTHON installed"
     echo "For the installation, you need an empty server with Ubuntu 22.04 or Debian 11 with root access - https://www.cloudpanel.io/docs/v2/technology-stack/"
+    exit
 fi
 
 if [[ $PYTHON3 ]]; then
     echo "You have $PYTHON3 installed"
     echo "For the installation, you need an empty server with Ubuntu 22.04 or Debian 11 with root access - https://www.cloudpanel.io/docs/v2/technology-stack/"
+    exit
 fi
 
 if [[ $PHP ]]; then
     echo "You have $PHP installed"
     echo "For the installation, you need an empty server with Ubuntu 22.04 or Debian 11 with root access - https://www.cloudpanel.io/docs/v2/technology-stack/"
+    exit
 fi
 
 if [[ $REDIS ]]; then
     echo "You have $REDIS installed"
     echo "For the installation, you need an empty server with Ubuntu 22.04 or Debian 11 with root access - https://www.cloudpanel.io/docs/v2/technology-stack/"
+    exit
+fi
+
+if [[ $VARNISH ]]; then
+    echo "You have $VARNISH installed"
+    echo "For the installation, you need an empty server with Ubuntu 22.04 or Debian 11 with root access - https://www.cloudpanel.io/docs/v2/technology-stack/"
+    exit
 fi
 
 log_info "Install CloudPanel ..."
@@ -101,12 +123,14 @@ if [ "$DISTRO" = "debian" ]; then
       else
        echo "$MYSQL_VERSION"
        echo "Sorry there is nothing for MySQL/MariaDB v$MYSQL_VERSION"
+       exit
       fi
       
       echo "You can now access CloudPanel via Browser: http://$IP:8443"      
   
     else
        echo "Sorry $DISTRO v$VERSION is not supported."
+       exit
     fi
     
 elif [ "$DISTRO" = "ubuntu" ]; then
@@ -144,14 +168,17 @@ elif [ "$DISTRO" = "ubuntu" ]; then
       else
        echo "$MYSQL_VERSION"
        echo "Sorry there is nothing for MySQL/MariaDB v$MYSQL_VERSION"
+       exit
       fi
       
       echo "You can now access CloudPanel via Browser: http://$IP:8443"      
   
     else
        echo "Sorry $DISTRO v$VERSION is not supported."
+       exit
     fi
 else
    echo "$DISTRO v$VERSION"
    echo "Sorry this is not for you - https://www.cloudpanel.io/docs/v2/requirements/"
+   exit
 fi
